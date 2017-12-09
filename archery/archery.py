889516e3869bfeb258f31b8ca9beae523b64b8d8
NoUnique <kofmap@gmail.com>
2017-12-09 17:46:00 +0900

    Update archery.DIM and archery.DIA

--- archery/archery.py
+++ archery/archery.py
@@ -1,7 +1,7 @@
 import types
 
-DIM = 400
-DIA = 80.0
+DIM = 400 # pixels
+DIA = 800 # mm
 
 def _draw_circle(self, r, **kwargs):
     r = r / DIA * DIM
@@ -34,17 +34,17 @@ def draw_scoreboard(canvas):
     canvas.draw_circle = types.MethodType(_draw_circle, canvas)
     canvas.draw_horizon= types.MethodType(_draw_horizon, canvas)
     canvas.draw_vertical= types.MethodType(_draw_vertical, canvas)
-    canvas.draw_circle(r=39.9, fill="white", outline="black", width=0.2)
-    canvas.draw_circle(r=35.9, fill="white", outline="black", width=0.2)
-    canvas.draw_circle(r=32.0, fill="black")
-    canvas.draw_circle(r=27.9, fill="black", outline="white", width=0.2)
-    canvas.draw_circle(r=24.0, fill="#41b7c8")    
-    canvas.draw_circle(r=19.9, fill="#41b7c8", outline="black", width=0.2)
-    canvas.draw_circle(r=15.9, fill="#fd1b14", outline="black", width=0.2)
-    canvas.draw_circle(r=11.9, fill="#fd1b14", outline="black", width=0.2)
-    canvas.draw_circle(r=7.9, fill="#fff535", outline="black", width=0.2)
-    canvas.draw_circle(r=3.9, fill="#fff535", outline="black", width=0.2)
-    canvas.draw_circle(r=1.9, fill="#fff535", outline="black", width=0.1)
+    canvas.draw_circle(r=399, fill="white", outline="black", width=0.2)
+    canvas.draw_circle(r=359, fill="white", outline="black", width=0.2)
+    canvas.draw_circle(r=320, fill="black")
+    canvas.draw_circle(r=279, fill="black", outline="white", width=0.2)
+    canvas.draw_circle(r=240, fill="#41b7c8")    
+    canvas.draw_circle(r=199, fill="#41b7c8", outline="black", width=0.2)
+    canvas.draw_circle(r=159, fill="#fd1b14", outline="black", width=0.2)
+    canvas.draw_circle(r=119, fill="#fd1b14", outline="black", width=0.2)
+    canvas.draw_circle(r=79, fill="#fff535", outline="black", width=0.2)
+    canvas.draw_circle(r=39, fill="#fff535", outline="black", width=0.2)
+    canvas.draw_circle(r=19, fill="#fff535", outline="black", width=0.1)
     # draw cross
-    canvas.draw_horizon(l=0.4, fill="black", width=0.1)
-    canvas.draw_vertical(l=0.4, fill="black", width=0.1)
+    canvas.draw_horizon(l=4, fill="black", width=0.1)
+    canvas.draw_vertical(l=4, fill="black", width=0.1)
